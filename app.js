f81f6252306b952d6d9b737b65de7f66a7ea718e
skotivanwalker <skotivanwalker@gmail.com>
2020-04-07 12:35:29 -0700

    Added placeholder messaging/actions for deck.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('testing')
+
+
+
+const problemCardDeck = [
+	{message: 'Move Forward!', action: +1},
+	{message: 'Move Forward!', action: +1},
+	{message: 'Move Forward!', action: +1},
+	{message: 'Go Back!', action: -1},
+	{message: 'Go Back!', action: -1},
+	{message: 'Go Back!', action: -1},
+	{message: 'Lose a Turn!', action: loseTurn},
+	{message: 'Lose a Turn!', action: loseTurn},
+	{message: 'Lose a Turn!', action: loseTurn},
+	{message: 'Go Again!', action: goAgain},
+	{message: 'Go Again!', action: goAgain},
+	{message: 'Go Again!', action: goAgain},
+]
+
