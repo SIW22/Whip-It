c1c553c24b21c3c240194cd136dc7eb9f23018ec
skotivanwalker <skotivanwalker@gmail.com>
2020-04-09 09:19:39 -0700

    added code to main board.

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ console.log('testing')
 //localStorage.setItem('test','this is working');
 
 function playerSelect() {
-	if (document.getElementById(onePlayer).addEventListener('click')) { // on button click
+	if (document.getElementById(onePlayer).addEventListener('click', () => { // on button click
 		localStorage.setItem('numberOfPlayers:', 1); // store number of players
 		document.getElementsByClassName('colorPrompt').innerHTML = "PLAYER ONE, CHOOSE A COLOR"; // update text on page
 		colorSelect(); // run color select program
-	} else {
-		if (document.getElementsById(twoPlayer).addEventListener('click')) { //on button click
+	}) else 
+			(document.getElementsById(twoPlayer).addEventListener('click', () => { //on button click
 			localStorage.setItem('numberOfPlayers:', 2); // store number of players
 			document.getElementsByClassName('colorPrompt').innerHTML = "PLAYER ONE, CHOOSE A COLOR"; //update text on page
 			colorSelect(); // run color select function
 			localStorage.setItem('player1ColorChoice', 'arrNum'); // store color choice
-			//wait 10 seconds for input, then pick random color
+			// to add - wait 10 seconds for input, then pick random color
 			document.getElementsByClassName('colorPrompt').innerHTML = "PLAYER TWO, CHOOSE A COLOR"; //update text on page
 			colorSelect('arrNum'); // run color select function
 			localStorage.setItem('player2ColorChoice', 'arrNum'); // store color choice
-			//wait 10 seconds for input, then pick random color
-		}
+			//to add - wait 10 seconds for input, then pick random color
+		})
 	}
-}	
+
+const colorOptions = ['black','yellow','orange','red'];
 
 function colorSelect(arrNum) { 
 	if (document.querySelector('#black').addEventListener)('click', () => {
@@ -39,5 +40,3 @@ function colorSelect(arrNum) {
 		return colorOptions[3];
 	})
 }
-
-const colorOptions = ['black','yellow','orange','red'];
