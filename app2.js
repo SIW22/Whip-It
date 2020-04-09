1962ca2c87c42c1aa7c43a3b8c8d732d610d2723
skotivanwalker <skotivanwalker@gmail.com>
2020-04-08 23:12:23 -0700

    Started to rework class names.

--- app2.js
+++ app2.js
@@ -27,7 +27,7 @@ const dieSides = [
 
 const player1 = {
 	color: '',
-	currentSquare: '.square_0',
+	currentSquare: '.0.square_0',
 }
 
 const player2 = {
@@ -64,7 +64,7 @@ function onePlayerGameProtocol() {
 	// player piece moves or a card is drawn
 	// player must click to resolve card
 	// repeats until square 34 is reached
-	// 'lose a turn' cards are ignored
+	// 'lose a turn' cards are skipped in array (items 12-14)
 	// player one victory page "You Whipped it Good!"
 }
 
@@ -107,10 +107,26 @@ function loseTurn() { //triggers goAgain on otherplayer
 
 }
 
+let currentSquare = ('.0_square'); //starting square
+
 function movePlayer(playerNum, distance) { //moves spaces based on input
-	// player starts on square_0. and cannot back up from there.
-	//check if player is on square_0.
-	const currentSquare = ()
+	// player starts on square_0. and cannot back up from there;
+	if(currentSquare === '0_Square') { // check if player is on 0_square
+		return currentSquare;
+	} else 
+		let locNum = currentSquare.replace(".", ""); // remove period
+		let parsedNum = parseFloat(locNum, 10); // create number from string
+		if (parsedNum === 0) {
+			let divLoc = currentSquare;
+		} else {
+			let newLocNum = parsedNum + moveDistance // add distance from card or die
+			let divLoc = ('.' + newLocNum + '_square') // add number back to string
+			let newCurrentSquare = divLoc  // append to new div
+			document.getElementsByClassName(divLoc).append.getElementsByClassName(playerPiece1);
+
+}
+
+	currentSquare = ('.0_square')
 	if (currentSquare === 0) {
 		if (distance >= 0) {
 			//add distance to current appended square
@@ -119,24 +135,51 @@ function movePlayer(playerNum, distance) { //moves spaces based on input
 	}
 
 
+	let p1currentSq = playerOne[1];
+	let p2currentSq = playerTwo[1];
+
+
+let currentSquare = ('.0_square'); //starting square
+
+let locNum = currentSquare.replace(".", ""); // remove period
+let parsedNum = parseFloat(locNum, 10); // create number from string
+if (parsedNum === 0) {
+	let newLocNum = parsedNum + moveDistance // add distance from card or die
+	let sumLoc = (newLocNum + '_square') // add number back to string
+	let newCurrentSquare = sumLoc  
+
+
+
+
+/* 
+let currentSquare = ('.0_square'); //starting square
+
+let locNum = currentSquare.replace(".", ""); // remove period
+let parsedNum = parseFloat(locNum, 10); // create number from string
+let newLocNum = parsedNum + moveDistance // add distance from card or die
+let sumLoc = (parsedNum + '_square') // add number back to string
+let newCurrentSquare = sumLoc 
+ */
+
+
+let children = document.getElementsByClassName('gameBoard');
+	children.hasChildNodes());
+
 
 
 	// use int to move current player position on the board
 	// append player piece to different square based on int
 }
 
-document.getElementsByClassName(.forward).addEventListener() {
-	document.querySelector("#The-Shire").append(ul);
-}
+document.getElementsByClassName(p1forward).addEventListener() {
+	document.querySelector(nextParentNode).append(playerOne);
+} 
+
+
+
+/* Sound Effect */
 
 const whipCrack = new Audio(whipcrack.mp3)
-
-/* function whipIt() { //
-	// resolves active Problem Card
-	#whipItLogo.addEventListener('click', () => {})
-	whipCrack.play();
-	})
-} */
 
 let logoButton = document.getElementById(whipItLogo);
 
@@ -144,8 +187,55 @@ logoButton.addEventListener('click', () => {
 	whipCrack.play();
 	})
 
-// document.querySelector('button').addEventListener('click', function() {
-// 	$('h2').attr('id', 'name').append('Skot Walker');
-	
-// 	console.log("The Die Was Rolled!");
-// })
+
+	/* Game Play */
+/* 
+game loads, and prompts if there will be one or two players:
+player one chooses a player piece color
+player two chooses a player piece color (only if Two Player was selected)
+check if color was already selected by player one, if yes
+prompt w/ that color has already been selected, please choose a different color.
+**Nice to have: highlight overlay on mouse hover, maybe increase size of button a bit
+**Maybe the pieces jiggle a little bit while waiting?
+**Gray out previously selected color
+
+player piece(s) appear at 'start'.
+if two players, player pieces appear side by side
+check if more than one object appended to Start Div
+adjust placement if yes
+
+random die roll: One, Two, or Problem Card
+On One, move player piece to next Square div
+append player piece to next div in series
+
+On Two, move player piece to next Square div
+append player piece to second div in series
+
+On Problem Card, player does not move
+A Random Card message is drawn from the deck
+
+Move Ahead! - same as a One die roll
+must click the logo button in the center of the board to continue.
+
+Move Back! - move player piece to previous Square div
+append player piece to previous div in series
+unless you are on start, as you can't move further back.
+must click the logo button in the center of the board to continue.
+
+Lose a turn! (Only on Two Player) 
+The player's turn ends immediately, and the other player
+takes two turns in a row.
+must click the logo button in the center of the board to continue.
+
+Go Again! (Only on Two Player)
+the player can roll again immediately
+must click the logo button in the center of the board to continue.
+
+game play continues until one player reaches the Finish Square.
+open finish dialog and ask to play again w/ button
+
+** Nice to have: sound effect on problem card resolution.
+
+**Nice to have: active player is highlighted by adding a glow to the square
+their piece is on. 
+*/
